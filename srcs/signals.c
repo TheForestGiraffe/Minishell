17e1e1a261a6764a8ebd7d5d1e8c8081790b6794
Pedro Lima <pedrolima.ep@gmail.com>
2025-10-06 17:36:20 +0200

    #11 WIP:  implement setup_signals()

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,16 +6,19 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 13:30:56 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/10/06 13:48:23 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/10/06 17:31:43 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "signals.h"
+#include <signal.h>
+#include <unistd.h>
 #include <readline/readline.h>
+#include "minishell.h"
 
 void    setup_signals(void)
 {
-    //TODO: implement
+    signal(SIGINT, handle_sigint);
+    signal(SIGQUIT, SIG_IGN);
 }
 
 void    handle_sigint(int signal)
