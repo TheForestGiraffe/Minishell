31b9625811e9d2277ad90524388866df52ea51af
pecavalc <pecavalc@student.42berlin.de>
2025-10-17 14:03:31 +0200

    WIP: Implement handle redirection

--- src/parser/fn_build_cmd_lst.c
+++ src/parser/fn_build_cmd_lst.c
@@ -6,17 +6,19 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:57:06 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/10/16 15:54:40 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/10/17 14:03:31 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "local_parser.h"
 
+static int	handle_redirection(t_cmd *cur_cmd, t_cmd *cur_token);
+
 t_cmd	*build_cmd_lst(t_token *token_lst)
 {
-	t_cmd 	*cmd_lst;
-	t_cmd 	*cur_cmd;
-	t_token *cur_token;
+	t_cmd	*cmd_lst;
+	t_cmd	*cur_cmd;
+	t_token	*cur_token;
 
 	if (!token_lst)
 		return (NULL);
@@ -30,7 +32,7 @@ t_cmd	*build_cmd_lst(t_token *token_lst)
 	while (cur_token)
 	{
 		if ((cur_token->type == WORD) || (cur_token->type == S_QT) || 
-				(cur_token->type == D_QT))
+			(cur_token->type == D_QT))
 		{
 			if (add_argv(cur_token, cur_cmd) == -1)
 			{
@@ -47,11 +49,18 @@ t_cmd	*build_cmd_lst(t_token *token_lst)
 				return (NULL);
 			}
 		}
-		else if (cur_token->type == INPUT)
-		{	
-			// TODO
+		else if ((cur_token->type == INPUT) || (cur_token->type == OUTPUT)
+			|| (cur_token->type == RINPUT) || (cur_token->type == ROUTPUT))
+		{
+			handle_redirection(cur_cmd, cur_token);
 		}
 		cur_token = cur_token->next;
 	}
 	return (cmd_lst);
+}
+
+int	handle_redirection(t_cmd *cur_cmd, t_cmd *cur_token)
+{
+	if (cur_token->)
+	return (1);
 }
