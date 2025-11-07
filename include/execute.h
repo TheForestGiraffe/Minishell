191cf0d763fe1a8632fad3332b787bbc5a9f2958
pecavalc <pecavalc@student.42berlin.de>
2025-11-07 17:43:30 +0100

    Remove unnecessary includes

--- include/execute.h
+++ include/execute.h
@@ -6,15 +6,14 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 19:41:33 by kalhanaw          #+#    #+#             */
-/*   Updated: 2025/11/07 10:35:25 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/11/07 17:43:03 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTE_H
 # define EXECUTE_H
 
-# include <stdbool.h>
-# include "local_execute.h"
+# include "parser.h"
 
 int	execute(t_cmd *cmd_lst, char **envp);
 
