48d24623480977245caf4f86da23beddda102ca5
Pedro Lima <pedrolima.ep@gmail.com>
2025-11-15 22:05:08 +0100

    WIP: Implement builtin_exit

--- src/builtin/fn_builtin_exit.c
+++ src/builtin/fn_builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 14:13:04 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/11/13 20:41:15 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/11/15 22:01:48 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,43 @@
 #include "types.h"
 #include <stdlib.h>
 
-int	builtin_exit(t_exec_context *ctxt)
+static void	handle_exit_with_one_arg(t_exec_context *exec_context)
 {
-	(void)ctxt;
-	// int		ret;
-	// int		nr_args;
-	// t_cmd	*cur_arg;
+	int		ret;
 
-	// if (!ctxt || !ctxt->cmd_lst)
-	// 	exit(EXIT_FAILURE);
-	// nr_args = 0;
-	// while (ctxt->cmd_lst->argv->next)
-	// {
-	// 	cur_arg = ctxt->cmd_lst->argv->next;
-	// 	nr_args++;
-	// }
-	// if (ctxt->there_is_one_cmd_and_is_builtin_exit)
-	// 	ft_printf("exit\n");
-	// if (nr_args == 0)
-	// 	exit(ctxt->exit_state);
-	// else if (nr_args == 1)
-	// {
-	// 	ret = ft_strtol(ctxt->cmd_lst->argv->next, ctxt->exit_state);
-	// 	if (ret == -1)
-	// 	{
-	// 		ft_printf("exit: %s: numeric argument required\n",
-	// 			ctxt->cmd_lst->argv->next);
-	// 		exit(2);
-	// 	}
-	// 	exit(ctxt->exit_state);
-	// }
-	// else
-	// {
-	// 	ft_printf("exit: too many arguments\n");
-	// 	exit(1);
-	// }
+	ret = ft_strtol(exec_context->cmd_lst->argv->next->content,
+			exec_context->exit_state);
+	if (ret == -1)
+	{
+		ft_printf("exit: %s: numeric argument required\n",
+			exec_context->cmd_lst->argv->next);
+		exit(2);
+	}
+	ft_printf("exit\n");
+	exit(exec_context->exit_state);
+}
+
+int	builtin_exit(t_exec_context *exec_context)
+{
+	int		nr_args;
+	t_token	*cur_arg;
+
+	if (!exec_context || !exec_context->cmd_lst)
+		exit(EXIT_FAILURE);
+	nr_args = 0;
+	while (exec_context->cmd_lst->argv->next)
+	{
+		cur_arg = exec_context->cmd_lst->argv->next;
+		nr_args++;
+	}
+	if (nr_args == 0 && (exec_context->main_pid == getpid()))
+	{
+		ft_printf("exit\n");
+		exit(exec_context->exit_state);
+	}
+	if (nr_args == 1)
+		handle_exit_with_one_arg(exec_context);
+	ft_printf("exit: too many arguments\n");
+	exit(1);
 	return (1);
 }
