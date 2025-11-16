d9a874e8137d8077bf049638f05e7cbc72089deb
TheForestGiraffe <121735952+TheForestGiraffe@users.noreply.github.com>
2025-11-16 23:18:25 +0100

    Implement function to copy  envp

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 11:54:30 by plima             #+#    #+#             */
-/*   Updated: 2025/11/15 20:22:05 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/11/16 22:34:46 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,47 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <unistd.h>
+
+static int	copy_env_line(char **envp, char **new_envp, size_t i)
+{
+	new_envp[i] = ft_strdup(envp[i]);
+	if (!new_envp[i])
+	{
+		while (i > 0)
+			free(new_envp[--i]);
+		free(new_envp);
+		perror("@copy_environment.ft_strdup");
+		return (-1);
+	}
+	return (1);
+}
+
+static char	**copy_env(char **envp)
+{
+	char	**new_envp;
+	size_t 	i;
+	size_t	length;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	length = i;
+	new_envp = (char **)malloc((length + 1) * sizeof(char *));
+	if (!new_envp)
+	{
+		perror("@copy_environment.malloc");
+		return (NULL);
+	}
+	i = 0;
+	while (i < length)
+	{
+		if (copy_env_line(envp, new_envp, i) == -1)
+			return (NULL);
+		i++;
+	}
+	new_envp[i] = NULL;
+	return (new_envp);
+}
 
 static int	read_parse_and_execute(t_exec_context *exec_context)
 {
@@ -52,7 +93,9 @@ int	main(int argc, char **argv, char **envp)
 	atexit(enable_ctrl_chars_printing);
 	(void)argc;
 	(void)argv;
-	exec_context.envp = envp;
+	exec_context.envp = copy_env(envp);
+	if (!exec_context.envp)
+		return (-1);
 	exec_context.exit_state = 0;
 	exec_context.cmd_lst = NULL;
 	exec_context.main_pid = getpid();
